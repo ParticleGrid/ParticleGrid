f32c2a4baac0bf8d77e7ae4695ae16b15e38fa11
Shehtab Zaman <szaman5@binghamton.edu>
2021-12-02 02:14:16 -0500

    Updated discretization with passing Unit tests

--- src/discretizer.cpp
+++ src/discretizer.cpp
@@ -54,8 +54,6 @@ grid_to_coords(npcarray grid,
   const float lr = 0.01;
   while(iteration < max_iterations){
     // Generate the grid with the estimated points
-    // py::print("----------------------------------------------------------------");
-    // py::print("Before update: ", iteration, *estimated_coords);
     coord_to_grid_forward(estimated_coords,
                           grid_size,
                           num_channels,
@@ -90,13 +88,100 @@ grid_to_coords(npcarray grid,
     update_coords(estimated_coords,
                   estimated_coords_grad_ptr,
                   lr);
-    // py::print("After update: ", iteration,  *estimated_coords);
-    // py::print("----------------------------------------------------------------");
     iteration++;
   }
   return estimated_coords;  
 }
 
+py::array_t<float> 
+optimize_coords(npcarray grid,
+               const npcarray estimated_coords,
+               const float variance,
+               const float threshold, 
+               const float max_iterations){
+  // Following line assumes a 4D grid, which is while valid
+  // not particularly extensible
+
+  const std::vector<ssize_t> grid_dims{grid.shape(), grid.shape()+4}; 
+  const int num_channels = grid_dims[0];
+  const int grid_size = grid_dims[1];
+
+  const int num_atoms = estimated_coords.shape(0);
+
+  std::vector<ssize_t> coords_shape = {(ssize_t)num_atoms, 4};
+  npcarray coords = py::array_t<float>(coords_shape);
+  auto* coords_ptr = (float*)coords.request().ptr;
+
+  // Copy over the estimated data
+  std::memcpy(coords_ptr, (float*)estimated_coords.request().ptr, num_atoms*4*sizeof(float));
+
+  // The offset is the number of grid points per channel. It's used as a stride for 
+  // traversing through the grid pointer
+
+  const int offset = std::accumulate(grid_dims.begin()+1, grid_dims.end(), 1, std::multiplies<int>());
+  const int num_grid_points = num_channels * offset;
+
+  py::print("Num Channels",num_channels, "Offset ",offset);
+
+  const float* grid_ptr = (float*)grid.request().ptr;
+
+  bool needs_optimize = true;
+  auto iteration = 0; 
+
+  // Set up containers for SGD
+
+  npcarray predicted_grid = py::array_t<float>(grid_dims);  
+  float* predicted_grid_ptr = (float*)predicted_grid.request().ptr;
+  std::memset(predicted_grid_ptr, 0, num_grid_points);
+
+  float grid_points_grad[num_grid_points];
+  float estimated_coords_grad_ptr[coords.shape(0)*3];
+
+  // Set up constants for grid
+
+  const float oc = INTEGRAL_NORMALIZATION_3D;
+  const float ic = (float)(1/(SQRT_2 * variance));
+  const float lr = 0.01;
+  while(iteration < max_iterations){
+    // Generate the grid with the estimated points
+    coord_to_grid_forward(coords,
+                          grid_size,
+                          num_channels,
+                          ic,
+                          oc,
+                          predicted_grid_ptr);
+
+    // Calculate the error between the predicted 
+    // grid and the original grid.
+    // Concurrently calculate the gradient with 
+    // respect the grid elements and signal if backward 
+    // pass is needed
+    squared_error_backward(grid_ptr,
+                           predicted_grid_ptr,
+                           grid_points_grad,
+                           num_grid_points,
+                           threshold,
+                           needs_optimize);
+    if (!needs_optimize){
+      py::print("Stopping updates");
+      break;
+    }
+    // If the backward pass is needed, update the  
+    // coordinates with the calculated gradients 
+
+    generate_grid_backward(coords,
+                           grid_points_grad,
+                           estimated_coords_grad_ptr,
+                           grid_size,
+                           variance);
+
+    update_coords(coords,
+                  estimated_coords_grad_ptr,
+                  lr);
+    iteration++;
+  }
+  return coords;
+}
 
 PYBIND11_MODULE(Discretizer, m) {
   m.doc() = "Retrieve atom set from grids";
@@ -106,4 +191,12 @@ PYBIND11_MODULE(Discretizer, m) {
         py::arg("variance") = 0.04,
         py::arg("tolerance") = 2.0e-4,
         py::arg("max_iterations") = 10000);
+  m.def("optimize_coords", &optimize_coords,
+        "Optimize a set of estimated coordinates to a grid",
+        py::arg("grid"),
+        py::arg("estimated_coords"),
+        py::arg("variance"),
+        py::arg("tolerance") = 2.0e-4,
+        py::arg("max_iterations") = 10000);
 }
+
--- tests/test_discretizer.py
+++ tests/test_discretizer.py
@@ -4,7 +4,7 @@ def test_import():
 
 def test_discretizer_multi_atom_multi_channel():
     from ParticleGrid import coord_to_grid
-    from Discretizer import grid_to_coords
+    from Discretizer import optimize_coords
     import numpy as np
 
     test_point = np.array([[0, 0.5, 0.5, 0.5],
@@ -17,49 +17,23 @@ def test_discretizer_multi_atom_multi_channel():
                          num_channels=2,
                          grid_size=32,
                          variance=0.05)
-    estimated_coords = grid_to_coords(grid)
-    print(estimated_coords, "\n",test_point)
+
+    some_noise = np.random.rand(2,3) * (0.1)
+
+
+    noisy_approximation = test_point.copy()
+    noisy_approximation[:,1:] = noisy_approximation[:,1:]  + some_noise
+
+    print("Noisy Approximation: \n",noisy_approximation)
+
+    estimated_coords = optimize_coords(grid,
+                                       noisy_approximation,
+                                       variance=0.05, 
+                                       tolerance=3e-7)
+
+    print("Optimized coordinations: \n",estimated_coords, "\n",test_point)
     assert np.allclose(test_point, estimated_coords)
-
-def test_discretizer_multi_atom_single_channel():
-    from ParticleGrid import coord_to_grid
-    from Discretizer import grid_to_coords
-    import numpy as np
-
-    test_points = np.array([[0, 0.5, 0.5, 0.5],
-                            [0, 0.25, 0.25, 0.25]])
-
-    grid = coord_to_grid(test_points,
-                         width=1,
-                         height=1,
-                         depth=1,
-                         num_channels=2,
-                         grid_size=32,
-                         variance=0.05)
-    print(np.sum(grid))
-    estimated_coords = grid_to_coords(grid)
-    print(estimated_coords, "\n", test_points)
-    assert np.allclose(test_points, estimated_coords) 
-
-def test_discretizer_single_atom_single_channel():
-    from ParticleGrid import coord_to_grid
-    from Discretizer import grid_to_coords
-    import numpy as np
-
-    test_points = np.array([[0, 0.5, 0.5, 0.5]])
-
-    grid = coord_to_grid(test_points,
-                         width=1,
-                         height=1,
-                         depth=1,
-                         num_channels=1,
-                         grid_size=32,
-                         variance=0.05)
-    print("Sum of original grid: ", np.sum(grid))
-    estimated_coords = grid_to_coords(grid, 0.05, tolerance=1e-7, max_iterations=10000)
-    print(estimated_coords, "\n", test_points)
-    assert np.allclose(test_points, estimated_coords, rtol=1e-04, atol=1e-6) 
 
 
 if __name__ == '__main__':
-    test_discretizer_single_atom_single_channel()
+    test_discretizer_multi_atom_multi_channel()
